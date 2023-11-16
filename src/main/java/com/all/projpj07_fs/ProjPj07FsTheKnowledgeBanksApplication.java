d2eb65750d895976230b2a14367ff0b8051210ab
lilwxs <124787909+lilwxs@users.noreply.github.com>
2023-11-16 15:06:35 +0700

    UPLOAD

---
+++ src/main/java/com/all/projpj07_fs/ProjPj07FsTheKnowledgeBanksApplication.java
@@ -0,0 +1,13 @@
+package com.all.projpj07_fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjPj07FsTheKnowledgeBanksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjPj07FsTheKnowledgeBanksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/all/projpj07_fs/ProjPj07FsTheKnowledgeBanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all.projpj07_fs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjPj07FsTheKnowledgeBanksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
